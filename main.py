0253bea5188daf463bba431b1aaf19748e5ee074
AleX04Nov <35816182+AleX04Nov@users.noreply.github.com>
2025-02-26 19:36:24 +0100

    change the order of example commands in `main.py`

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from __motor_controll__ import *
 List of available commands:
 
     print("This is my message")
+    
+    led_on()
     count()
     sleep(5)
-
-    led_on()
     led_off()
 
     read_sensor_temperature()
